1444af2eebcac480fbddca8dece840c341004a8b
wangkaiwd <1162486176@qq.com>
2019-03-26 23:41:43 +0800

    feat: 路由定义以及侧边栏配置

--- src/layouts/leftNav/menuConfig.js
+++ src/layouts/leftNav/menuConfig.js
@@ -1,7 +1,6 @@
 const menuConfig = [
-  {
-    title: 'vue插槽', index: '/slot', icon: '',
-  }
+  { title: 'vue插槽', index: '/slot' },
+  { title: 'vue自定义指令', index: '/directive' }
 ];
 
 export default menuConfig;
--- src/router/index.js
+++ src/router/index.js
@@ -15,5 +15,11 @@ export default new Router({
       component: getComponent('vueSlot'),
       meta: { title: '插槽' }
     },
+    {
+      path: '/directive',
+      name: 'directive',
+      component: getComponent('vueDirective'),
+      meta: { title: '自定义指令' }
+    }
   ]
 });
