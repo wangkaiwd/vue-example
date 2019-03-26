0d4a1bf988e85568a3e526139c8399c6e0c095d3
wangkaiwd <1162486176@qq.com>
2019-03-26 23:36:57 +0800

    feat: 建立directive目录

---
+++ src/views/vueDirective/demo01.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="directive-demo01">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DirectiveDemo01'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .directive-demo01 {
+
+  }
+</style>
---
+++ src/views/vueDirective/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="vue-directive"></div>
+</template>
+
+<script>
+  export default {
+    name: 'VueDirective'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .vue-directive {
+
+  }
+</style>
