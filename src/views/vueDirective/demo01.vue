3f7ec4fa3b7bc45241e8893f1e825d340a74f748
wangkaiwd <1162486176@qq.com>
2019-03-27 19:20:07 +0800

    feat: 自定义指令的参数学习

--- src/views/vueDirective/demo01.vue
+++ src/views/vueDirective/demo01.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="directive-demo01">
     <!--自定义指令：实现当进入刚进入页面时，input获得焦点-->
-    <input v-focus type="text" placeholder="请输入内容">
+    <input v-focus:foo="123" type="text" placeholder="请输入内容">
   </div>
 </template>
 
@@ -17,12 +17,30 @@
         //  update: 所有组件的VNode更新时调用，但是可能发生在其子VNode更新之前。指令的值可能发生了变化，也可能没有
         //  componentUpdated: 指令所在的VNode及其子VNode全部更新后调用
         //  unbind: 只调用一次，指令与元素解绑时调用
-        inserted (el) {
-          el.focus();
+        /*
+        * 参数：
+        *   1. el: 指令所绑定的元素，可以用来直接操作DOM
+        *   2. binding: 包含指令信息的对象
+        *   3. vnode: Vue编译生成的虚拟节点
+        * */
+        inserted (el, binding, vnode) {
+          // console.log('el', el)
+          // console.log('binding', binding)
+          // // {
+          // //   arg: "foo"
+          // //   def: {inserted: ƒ}
+          // //   expression: "123"
+          // //   modifiers: {}
+          // //   name: "focus"
+          // //   rawName: "v-focus:foo"
+          // //   value: 123
+          // // }
+          // console.log('vnode', vnode)
+          el.focus()
         }
       }
     }
-  };
+  }
 </script>
 
 <style lang="scss" scoped>
