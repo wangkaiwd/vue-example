9cc847447b9bd4239645c1aec67ab3751067782a
wangkaiwd <1162486176@qq.com>
2019-04-13 23:37:33 +0800

    feat: 删除断点功能

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,10 +1,13 @@
 <template>
   <el-container class="layout">
-    <el-aside width="200px" class="layout-aside">Aside</el-aside>
+    <el-aside :width="`${sideWidth}px`" class="layout-aside">Aside</el-aside>
     <el-container>
-      <el-header class="layout-header"><i class="el-icon-tickets layout-icon"></i>Header</el-header>
+      <el-header class="layout-header">
+        <i @click="onIconClick" :class="{collapsed}" class="el-icon-tickets layout-icon"></i>
+        Header
+      </el-header>
       <el-main>Main</el-main>
-      <el-footer>Footer</el-footer>
+      <el-footer class="layout-footer">Footer</el-footer>
     </el-container>
   </el-container>
 </template>
@@ -13,11 +16,39 @@
   export default {
     name: 'LayoutDemo',
     mounted () {
-      window.addEventListener('resize', this.calcSideSize);
+      // this.calcSideSize();
+      // window.addEventListener('resize', this.calcSideSize);
+    },
+    data () {
+      return {
+        collapsed: false,
+        timerId: null
+      };
+    },
+    computed: {
+      sideWidth () {
+        return this.collapsed ? 40 : 200;
+      }
     },
     methods: {
-      calcSideSize () {
-
+      // calcSideSize () {
+      //   const width = document.documentElement.offsetWidth;
+      //   if (this.timerId) {
+      //     clearTimeout(this.timerId);
+      //     this.timerId = null;
+      //   }
+      //   this.timerId = setTimeout(() => {
+      //     if (width < 768) {
+      //       this.sideWidth = 40;
+      //       this.collapsed = true;
+      //     } else {
+      //       this.sideWidth = 200;
+      //       this.collapsed = false;
+      //     }
+      //   }, 100);
+      // },
+      onIconClick () {
+        this.collapsed = !this.collapsed;
       }
     }
   };
@@ -26,16 +57,27 @@
 <style lang="scss" scoped>
   .layout {
     border: 1px solid red;
+    height: 600px;
     &-aside {
       border: 1px solid green;
+      transition: all .4s;
     }
     &-header {
       display: flex;
       align-items: center;
+      background-color: pink;
     }
     &-icon {
       font-size: 20px;
       color: blue;
+      cursor: pointer;
+      transition: all .4s;
+      &.collapsed {
+        transform: rotate(90deg);
+      }
+    }
+    &-footer {
+      background-color: skyblue;
     }
   }
 </style>
