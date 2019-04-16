d21abbb89d54d125afaf819629c5b041481651be
wangkaiwd <1162486176@qq.com>
2019-04-16 22:45:16 +0800

    feat: 尝试render jsx 的使用

--- src/views/vueRender/index.vue
+++ src/views/vueRender/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="vue-render">
-    render
+
   </div>
 </template>
 
 <script>
   export default {
-    name: 'VueRender'
+    name: 'VueRender',
+    data () {
+      return {
+        list: [
+          { key: 1, name: '小明' },
+          { key: 2, name: '小红' }
+        ]
+      };
+    },
+    methods: {}
   };
 </script>
 
