c334f91cd4491532a89a72c87a3b66f8fda8c502
wangkaiwd <1162486176@qq.com>
2019-04-17 23:27:28 +0800

    feat: render 和 jsx尝试，感觉并不好用

--- src/views/vueTable/index.vue
+++ src/views/vueTable/index.vue
@@ -1,27 +1,19 @@
-<template>
+<!--<template>
   <el-table
     :data="tableData"
     style="width: 100%">
     <el-table-column
-      prop="name"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="age"
-      label="年龄"
-      width="180"
-    >
-    </el-table-column>
-    <el-table-column
-      prop="email"
-      label="邮箱"
+      v-for="item in columns"
+      :prop="item.prop"
+      :label="item.label"
+      :key="item.prop"
+      :width="item.width"
     >
     </el-table-column>
   </el-table>
-</template>
+</template>-->
 
-<script>
+<script type="text/jsx">
   import data from 'mock';
 
   export default {
@@ -38,7 +30,11 @@
           {
             prop: 'age',
             label: '年龄',
-            width: '180'
+            width: '180',
+            render: (obj) => {
+              console.log(obj);
+              return <span>render测试</span>;
+            }
           },
           {
             prop: 'email',
@@ -46,6 +42,31 @@
           }
         ]
       };
+    },
+    render () {
+      return (
+        <el-table
+          data={this.tableData}
+          style="width: 100%"
+        >
+          {this.columns.map((item, i) => {
+            return (
+              <el-table-column
+                prop={item.prop}
+                label={item.label}
+                width={item.width}
+                key={item.prop}
+              >
+                {item.render ?
+                  item.render({ column: item, row: this.tableData[i], index: i })
+                  :
+                  this.tableData[i][item.prop]
+                }
+              </el-table-column>
+            );
+          })}
+        </el-table>
+      );
     }
   };
 </script>
