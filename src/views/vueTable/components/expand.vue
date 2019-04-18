1e84a96bbc57ada1c29806ffd0cd7fade1c48102
wangkaiwd <1162486176@qq.com>
2019-04-18 13:07:48 +0800

    feat: 使用function组件

---
+++ src/views/vueTable/components/expand.vue
@@ -0,0 +1,23 @@
+<script>
+  export default {
+    name: 'TableExpand',
+    functional: true, // 函数式组件
+    props: {
+      row: Object,
+      render: Function,
+      index: Number,
+      columns: {
+        type: Object,
+        default: () => ({})
+      }
+    },
+    render (h, ctx) {
+      const { row, index, columns, render } = ctx.props;
+
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
